20300400282da52b24e3d449851e4fe7a1208539
Nathan Nelson <nathan.nelson@pearson.com>
2015-02-09 15:17:16 -0700

    Added method to allow setting the Base URL

--- karma-read-json.js
+++ karma-read-json.js
@@ -1,27 +1,36 @@
 (function(window, document, undefined) {
     'use strict';
 
-    window.readJSON = function(url) {
-        var xhr = new XMLHttpRequest();
-        var json = null;
+    window.KarmaJSONReader = function(baseUrl) {
+        var baseUrl = baseUrl || 'test/data/';
+        
+        this.setBaseUrl = function(_baseUrl_) {
+            baseUrl = _baseUrl_;
+        };
+        
+        this.readJSON = function(filename) {
+            var xhr = new XMLHttpRequest();
+            var json = null;
+            var url = baseUrl + filename;
 
-        xhr.open('GET', url, false);
+            xhr.open('GET', url, false);
 
-        xhr.onload = function() {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    json = JSON.parse(xhr.responseText);
-                } else {
-                    console.error('readJSON - ', url, xhr.statusText);
+            xhr.onload = function() {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        json = JSON.parse(xhr.responseText);
+                    } else {
+                        console.error('readJSON - ', url, xhr.statusText);
+                    }
                 }
-            }
-        };
+            };
 
-        xhr.onerror = function() {
-            console.error('readJSON - ', url, xhr.statusText);
-        };
+            xhr.onerror = function() {
+                console.error('readJSON - ', url, xhr.statusText);
+            };
 
-        xhr.send(null);
-        return json;
+            xhr.send(null);
+            return json;
+        };
     };
 })(window, document);
